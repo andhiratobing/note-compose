ea6f21ef1b8174efed1dcd2886f3eebec415b1d0
ratobing <ratobing6@gmail.com>
2021-11-05 11:12:21 +0700

    add class AddEditNoteEvent.kt

---
+++ app/src/main/java/com/andhiratobing/notecompose/feature_note/presentation/add_edit_note/AddEditNoteEvent.kt
@@ -0,0 +1,14 @@
+package com.andhiratobing.notecompose.feature_note.presentation.add_edit_note
+
+import androidx.compose.ui.focus.FocusState
+
+sealed class AddEditNoteEvent {
+    data class EnterTitle(val value: String): AddEditNoteEvent()
+    data class ChangeTitleFocus(val focusState: FocusState): AddEditNoteEvent()
+    data class EnteredContent(val value: String): AddEditNoteEvent()
+    data class ChangeContentFocus(val focusState: FocusState): AddEditNoteEvent()
+    data class ChangeColor(val color: Int): AddEditNoteEvent()
+    object SaveNote: AddEditNoteEvent()
+
+
+}
