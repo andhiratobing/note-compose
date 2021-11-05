12a43a9859f3831eeb31586a08b980124914b429
ratobing <ratobing6@gmail.com>
2021-11-05 11:18:20 +0700

    add class Screen.kt

---
+++ app/src/main/java/com/andhiratobing/notecompose/feature_note/presentation/utils/Screen.kt
@@ -0,0 +1,8 @@
+package com.andhiratobing.notecompose.feature_note.presentation.utils
+
+sealed class Screen(
+    val route: String
+) {
+    object NotesScreen: Screen("notes_screen")
+    object AddEditNoteScreen: Screen("add_edit_note_screen")
+}
