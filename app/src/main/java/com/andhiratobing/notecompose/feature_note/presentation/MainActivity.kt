e9771db7513da53a291da7747985e3919b4ebcfa
ratobing <ratobing6@gmail.com>
2021-11-05 11:18:51 +0700

    configuration MainActivity for navigation.kt

---
+++ app/src/main/java/com/andhiratobing/notecompose/feature_note/presentation/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.andhiratobing.notecompose.feature_note.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.andhiratobing.notecompose.feature_note.presentation.add_edit_note.AddEditNoteScreen
+import com.andhiratobing.notecompose.feature_note.presentation.notes.NotesScreen
+import com.andhiratobing.notecompose.feature_note.presentation.utils.Screen
+import com.andhiratobing.notecompose.ui.theme.NoteComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@ExperimentalAnimationApi
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.NotesScreen.route
+                    ) {
+                        composable(route = Screen.NotesScreen.route) {
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNoteScreen.route +
+                                    "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                            )
+                        ) {
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(
+                                navController = navController,
+                                noteColor = color
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
